3ed451a9fed4f710e7f829c210a162ad83225bb0
baoray99 <bao.ray1999@gmail.com>
2021-03-24 13:26:51 +0700

    config enviroment to deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAGP1iYOAwIAXFekVKu6hh7dmRbbjOMNuY',
+    authDomain: 'admin-e-commerce-angular.firebaseapp.com',
+    projectId: 'admin-e-commerce-angular',
+    storageBucket: 'admin-e-commerce-angular.appspot.com',
+    messagingSenderId: '171786632583',
+    appId: '1:171786632583:web:1f5027b5574ae3b0222722',
+  },
 };
