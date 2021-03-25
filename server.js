92ea86c9edccf853ff1281cd7032afaadd50544e
baoray99 <bao.ray1999@gmail.com>
2021-03-25 21:15:08 +0700

    deployyy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static("./dist/admin-eCommerce"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/admin-eCommerce/" })
+  res.sendFile("index.html", { root: "./dist/admin-eCommerce/" })
 );
 
 // Start the app by listening on the default Heroku port
