a77212920512813ece23d5b674a9fe6ca58ced52
baoray99 <bao.ray1999@gmail.com>
2021-04-12 23:15:35 +0700

    add products page client, style and pagnitation

--- src/app/pages/products-by-category/products-by-category.component.ts
+++ src/app/pages/products-by-category/products-by-category.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product.models';
 import { ProductService } from 'src/app/services/product.service';
+import 'lodash';
+declare var _: any;
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-products-by-category',
@@ -11,14 +15,57 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductsByCategoryComponent implements OnInit {
   constructor(
     private productServices: ProductService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cartService: CartService
   ) {}
+  customOptions: OwlOptions = {
+    loop: false,
+    autoplay: false,
+    dots: false,
+    autoWidth: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  };
   products: Product[];
+  tempProducts: Product[];
+  pageIndex: number = 1;
+  startProduct = 0;
+  endProduct = 20;
+  rows: number[] = [1, 2, 3, 4];
   ngOnInit(): void {
     this.productServices
       .getLaptops(this.route.snapshot.queryParamMap.get('id_category'))
       .subscribe((res) => {
         this.products = res;
+        this.paginationProduct(this.startProduct, this.endProduct);
       });
+  }
+  paginationProduct(start: number, end: number) {
+    this.tempProducts = _.slice(this.products, start, end);
+  }
+  changeIndex(index) {
+    if (index - this.pageIndex > 0) {
+      this.startProduct += 20;
+      this.endProduct += 20;
+      this.paginationProduct(this.startProduct, this.endProduct);
+    } else {
+      this.startProduct -= 20;
+      this.endProduct -= 20;
+      this.paginationProduct(this.startProduct, this.endProduct);
+    }
+    this.pageIndex = index;
+  }
+  addToCart(item: Product) {
+    this.cartService.addToCart(item);
   }
 }
--- src/app/pages/products-by-category/products-by-category.module.ts
+++ src/app/pages/products-by-category/products-by-category.module.ts
@@ -4,12 +4,19 @@ import { CommonModule } from '@angular/common';
 import { ProductsByCategoryRoutingModule } from './products-by-category-routing.module';
 import { ProductsByCategoryComponent } from './products-by-category.component';
 
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { IconsProviderModule } from '../../icons-provider.module';
+
 
 @NgModule({
   declarations: [ProductsByCategoryComponent],
   imports: [
     CommonModule,
-    ProductsByCategoryRoutingModule
+    ProductsByCategoryRoutingModule,
+    NzPaginationModule,
+    CarouselModule,
+    IconsProviderModule
   ]
 })
 export class ProductsByCategoryModule { }
